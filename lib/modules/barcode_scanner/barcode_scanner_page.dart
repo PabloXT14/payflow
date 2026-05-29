ceb12a184a646a74b82bd9a268b1e736457414a1
Pablo Alan <pabloxt14@gmail.com>
2026-05-29 00:06:33 -0300

    feat: create app-bar on barcode page

--- lib/modules/barcode_scanner/barcode_scanner_page.dart
+++ lib/modules/barcode_scanner/barcode_scanner_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+import 'package:payflow/shared/themes/app_text_styles.dart';
 
 class BarcodeScannerPage extends StatefulWidget {
   const BarcodeScannerPage({super.key});
@@ -11,7 +13,14 @@ class _BarcodeScannerPageState extends State<BarcodeScannerPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Barcode Scanner')),
+      appBar: AppBar(
+        backgroundColor: AppColors.black,
+        title: Text(
+          'Escaneie o código de barras do boleto',
+          style: AppTextStyles.textMd.copyWith(color: AppColors.white),
+        ),
+        leading: BackButton(color: AppColors.white),
+      ),
       body: Center(child: Text('Barcode Scanner Page')),
     );
   }
