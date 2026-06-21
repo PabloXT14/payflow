58b19367790b9c74cf8cdf6f59085b7da224f27e
Pablo Alan <pabloxt14@gmail.com>
2026-06-20 21:29:19 -0300

    feat: pass barcode to insert_boleto_page

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -110,7 +110,7 @@ class _HomePageState extends State<HomePage> {
             GestureDetector(
               onTap: () {
                 // Navigator.pushNamed(context, '/barcode_scanner');
-                Navigator.pushNamed(context, '/insert_boleto');
+                Navigator.pushNamed(context, '/barcode_scanner');
               },
               child: Container(
                 width: 56,
